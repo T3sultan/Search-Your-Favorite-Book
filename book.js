bae6bfdc913e9771314690d76cfd322b9a130e02
“T3sultan” <incognito.soul011@gmail.com>
2021-09-02 00:12:17 +0600

    docs

--- book.js
+++ book.js
@@ -1,1 +1,22 @@
-console.log('book')
+
+const searchBook=()=>{
+    const inputField=document.getElementById('search-field');
+    // console.log(inputField)
+    const inputFieldText=inputField.value ;
+    // console.log(inputFieldText)
+    inputField.value='';
+
+    // fetch api
+    const url=`http://openlibrary.org/search.json?q=${inputFieldText}`;
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displayLoad(data.docs));  
+}
+
+//display show function
+const displayLoad=(books)=>{
+    // console.log(books)
+   books.forEach(book=>{
+       console.log(book.title)
+   })
+}
