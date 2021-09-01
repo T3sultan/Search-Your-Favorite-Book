ddf64f61708af38b16519e0c70859d33fa9120e4
“T3sultan” <incognito.soul011@gmail.com>
2021-09-02 03:34:44 +0600

    error

--- book.js
+++ book.js
@@ -1,22 +1,66 @@
 
-const searchBook=()=>{
-    const inputField=document.getElementById('search-field');
+//error 
+document.getElementById('error-message').style.display = 'none'
+
+//button clicked function
+const searchBook = () => {
+    const inputField = document.getElementById('search-field');
     // console.log(inputField)
-    const inputFieldText=inputField.value ;
+    const inputFieldText = inputField.value;
     // console.log(inputFieldText)
-    inputField.value='';
+    inputField.value = '';
+    document.getElementById('error-message').style.display = 'block'
+
+
 
     // fetch api
-    const url=`http://openlibrary.org/search.json?q=${inputFieldText}`;
+    const url = `https://openlibrary.org/search.json?q=${inputFieldText}`;
     fetch(url)
-    .then(res=>res.json())
-    .then(data=>displayLoad(data.docs));  
+        .then(res => res.json())
+        .then(data => displayLoad(data.docs))
+        .catch(error => displayError(error))
+}
+//error function
+const displayError = error => {
+    //console.log(error)
+    document.getElementById('error-message').style.display = 'none'
 }
 
+
 //display show function
-const displayLoad=(books)=>{
-    // console.log(books)
-   books.forEach(book=>{
-       console.log(book.title)
-   })
+const displayLoad = (books) => {
+    console.log(books)
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    // let countBooks=0;
+    books.forEach(book => {
+        //    console.log(book.title)
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `     
+    <div class="card h-100 shadow rounded-2 border-secondary "> 
+            <img 
+            style="width: 100%;height: 400px; 
+            class="figure-img img-fluid" 
+            src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" alt="...">
+             <div class="card-body">
+                 <h4 class='text-waring'><span>Book Name:</span> ${book.title}</h4>
+                <h6 class="card-title">Author Name: ${book.author_name[0]}</h6>
+                <h6 class="card-title">Publisher: ${book.publisher}</h6>
+                <p class="card-text">First publish: ${book.first_publish_year}</p>
+            
+            </div>
+    </div>
+
+       
+    `;
+        // countBooks=countBooks+books[book];
+
+
+        searchResult.appendChild(div)
+
+
+    })
+    //    console.log(countBooks)
 }
+
